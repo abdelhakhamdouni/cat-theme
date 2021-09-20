75e56801d6e2abb0fee30802f853127b236b6a36
abdelhakhamdouni <abdelhakhamdouni@sfr.fr>
2021-09-20 15:46:32 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// let myvideo = document.querySelector("#myvideo")
+
+// console.log(myvideo)
+// let i = 1
+
+// myvideo.addEventListener('ended', ()=>{
+//     i++
+//     if( i > 4) i = 1
+//     myvideo.src= `./videos/video${i}.mp4`
+//     myvideo.play()
+// } )
+
+document.querySelectorAll('.dropdown-toggle').forEach(ele =>{
+    ele.addEventListener('mouseenter', ()=>{
+        ele.parentNode.querySelector('.dropdown-menu').classList.toggle('show')
+    })
+})
+document.querySelectorAll('.dropdown-toggle').forEach(ele =>{
+    ele.addEventListener('mouseleave', ()=>{
+        ele.parentNode.querySelector('.dropdown-menu').classList.toggle('show')
+    })
+})
+
+document.querySelectorAll('.dropdown-menu').forEach(ele =>{
+    ele.addEventListener('mouseleave', function(){
+        this.classList.toggle('show')
+    })
+})
+document.querySelectorAll('.dropdown-menu').forEach(ele =>{
+    ele.addEventListener('mouseenter', function(){
+        this.classList.toggle('show')
+    })
+})
