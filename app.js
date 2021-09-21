7feb77019e2d678a4ccc2f405ac8e7c4ef100dad
abdelhakhamdouni <abdelhakhamdouni@sfr.fr>
2021-09-21 17:49:43 +0200

    .

--- app.js
+++ app.js
@@ -68,6 +68,40 @@ $(document).ready(function(){
             // instead of a settings object
           ]
     });
+    $('.mark_presence').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        infinite: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 6,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
   });
 
   document.querySelector('.navbar__manu_open').addEventListener('click', function(){
