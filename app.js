68a24f95b2a535c88b4514147426775e42e04fd8
abdelhakhamdouni <abdelhakhamdouni@sfr.fr>
2021-09-21 14:44:25 +0200

    .

--- app.js
+++ app.js
@@ -37,6 +37,39 @@ document.querySelectorAll('.dropdown-menu').forEach(ele =>{
 $(document).ready(function(){
     $('.news-carousel').slick({
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay: true,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
     });
   });
+
+  document.querySelector('.navbar__manu_toggler').addEventListener('click', function(){
+      document.querySelector('.navbar__mobile').classList.add('navbar-show')
+  })
